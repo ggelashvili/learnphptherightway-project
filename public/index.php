5f2d1264a33cecfedefe3764c118ce7de0438199
Gio <ggelashvili@gmail.com>
2021-02-10 21:25:37 -0500

    Instructions

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types = 1);
+
+$root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
+
+define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
+define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
+define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
+
+/* YOUR CODE (Instructions in README.md) */
---
+++ views/transactions.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Transactions</title>
+        <style>
+            table {
+                width: 100%;
+                border-collapse: collapse;
+                text-align: center;
+            }
+
+            table tr th, table tr td {
+                padding: 5px;
+                border: 1px #eee solid;
+            }
+
+            tfoot tr th, tfoot tr td {
+                font-size: 20px;
+            }
+
+            tfoot tr th {
+                text-align: right;
+            }
+        </style>
+    </head>
+    <body>
+        <table>
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Check #</th>
+                    <th>Description</th>
+                    <th>Amount</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- YOUR CODE -->
+            </tbody>
+            <tfoot>
+                <tr>
+                    <th colspan="3">Total Income:</th>
+                    <td><!-- YOUR CODE --></td>
+                </tr>
+                <tr>
+                    <th colspan="3">Total Expense:</th>
+                    <td><!-- YOUR CODE --></td>
+                </tr>
+                <tr>
+                    <th colspan="3">Net Total:</th>
+                    <td><!-- YOUR CODE --></td>
+                </tr>
+            </tfoot>
+        </table>
+    </body>
+</html>
