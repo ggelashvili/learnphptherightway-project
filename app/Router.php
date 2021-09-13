6f26b284ad5aab59667c9f741de4e107fb9a129a
Gio <ggelashvili@gmail.com>
2021-09-13 18:27:45 -0400

    Added tests for Router class

--- app/Router.php
+++ app/Router.php
@@ -8,7 +8,7 @@ use App\Exceptions\RouteNotFoundException;
 
 class Router
 {
-    private array $routes;
+    private array $routes = [];
 
     public function register(string $requestMethod, string $route, callable|array $action): self
     {
@@ -45,15 +45,13 @@ class Router
             return call_user_func($action);
         }
 
-        if (is_array($action)) {
-            [$class, $method] = $action;
+        [$class, $method] = $action;
 
-            if (class_exists($class)) {
-                $class = new $class();
+        if (class_exists($class)) {
+            $class = new $class();
 
-                if (method_exists($class, $method)) {
-                    return call_user_func_array([$class, $method], []);
-                }
+            if (method_exists($class, $method)) {
+                return call_user_func_array([$class, $method], []);
             }
         }
 
---
+++ tests/Unit/RouterTest.php
@@ -0,0 +1,130 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Tests\Unit;
+
+use App\Exceptions\RouteNotFoundException;
+use App\Router;
+use PHPUnit\Framework\TestCase;
+
+class RouterTest extends TestCase
+{
+    private Router $router;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->router = new Router();
+    }
+
+    /** @test */
+    public function it_registers_a_route(): void
+    {
+        $this->router->register('get', '/users', ['Users', 'index']);
+
+        $expected = [
+            'get' => [
+                '/users' => ['Users', 'index'],
+            ],
+        ];
+
+        $this->assertSame($expected, $this->router->routes());
+    }
+
+    /** @test */
+    public function it_registers_a_get_route(): void
+    {
+        $this->router->get('/users', ['Users', 'index']);
+
+        $expected = [
+            'get' => [
+                '/users' => ['Users', 'index'],
+            ],
+        ];
+
+        $this->assertSame($expected, $this->router->routes());
+    }
+
+    /** @test */
+    public function it_registers_a_post_route(): void
+    {
+        $this->router->post('/users', ['Users', 'store']);
+
+        $expected = [
+            'post' => [
+                '/users' => ['Users', 'store'],
+            ],
+        ];
+
+        $this->assertSame($expected, $this->router->routes());
+    }
+
+    /** @test */
+    public function there_are_no_routes_when_router_is_created(): void
+    {
+        $this->assertEmpty((new Router())->routes());
+    }
+
+    /**
+     * @test
+     * @dataProvider routeNotFoundCases
+     */
+    public function it_throws_route_not_found_exception(
+        string $requestUri,
+        string $requestMethod
+    ): void {
+        $users = new class() {
+            public function delete(): bool
+            {
+                return true;
+            }
+        };
+
+        $this->router->post('/users', [$users::class, 'store']);
+        $this->router->get('/users', ['Users', 'index']);
+
+        $this->expectException(RouteNotFoundException::class);
+        $this->router->resolve($requestUri, $requestMethod);
+    }
+
+    public function routeNotFoundCases(): array
+    {
+        return [
+            ['/users', 'put'],
+            ['/invoices', 'post'],
+            ['/users', 'get'],
+            ['/users', 'post'],
+        ];
+    }
+
+    /** @test */
+    public function it_resolves_route_from_a_closure(): void
+    {
+        $this->router->get('/users', fn() => [1, 2, 3]);
+
+        $this->assertSame(
+            [1, 2, 3],
+            $this->router->resolve('/users', 'get')
+        );
+    }
+
+    /** @test */
+    public function it_resolves_route(): void
+    {
+        $users = new class() {
+            public function index(): array
+            {
+                return [1, 2, 3];
+            }
+        };
+
+        $this->router->get('/users', [$users::class, 'index']);
+
+        $this->assertSame(
+            [1, 2, 3],
+            $this->router->resolve('/users', 'get')
+        );
+    }
+}
