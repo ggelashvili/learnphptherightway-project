cd79b8db72ef484ccfbfba4b5214d85e5f09f377
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-09-03 20:42:47 -0300

    Index concluído

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 $root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
 
@@ -8,4 +8,16 @@ define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
 define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
 
-/* YOUR CODE (Instructions in README.md) */
+require APP_PATH . "App.php";
+require APP_PATH . "helpers.php";
+
+$files = getTransactionFiles(FILES_PATH);
+
+$transactions = [];
+foreach($files as $file) {
+    $transactions = array_merge($transactions, getTransactions($file, "extractTransaction"));
+}
+
+$totals = calculateTotals($transactions);
+
+require VIEWS_PATH . "transactions.php";
