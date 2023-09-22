c6ebce86c069d507bef774fa886ca56a216d0d93
domantasslai <domantasslai@gmail.com>
2023-09-22 17:31:21 +0300

    .

---
+++ src/Transaction.php
@@ -0,0 +1,34 @@
+<?php
+
+
+class Transaction
+{
+    public function __construct(private float $amount, private string $description)
+    {
+
+    }
+
+    public function addTax(float $rate): self
+    {
+        if ($rate > 0) $this->amount += $this->amount * $rate / 100;
+
+        return $this;
+    }
+
+    public function applyDiscount(float $rate): self
+    {
+        if ($rate > 0) $this->amount -= $this->amount * $rate / 100;
+
+        return $this;
+    }
+
+    public function getAmount(): float
+    {
+        return $this->amount;
+    }
+
+    public function __destruct()
+    {
+        echo 'Destruct ' . $this->description . '<br/>';
+    }
+}
