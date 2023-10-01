54ac88914da936ba5d01cd5c71dfd81d0db8996a
Domantas <domantas.slaiciunas@dielektrik.com>
2023-10-01 15:52:04 +0300

    Magic Methods

--- public/index.php
+++ public/index.php
@@ -4,8 +4,28 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$service = new \App\DebtCollectionService();
+//$invoice = new \App\Invoice(15);
+//
+//$invoice->amount = 35;
+//
+//var_dump(isset($invoice->amount));
+//var_dump($invoice);
+//unset($invoice->amount);
+//
+//var_dump($invoice);
 
-echo $service->collectDebt(new \App\CollectionAgency()) . '<br>';
+//$invoice = new \App\Invoice();
+//\App\Invoice::process(1, 2, 3);
+//$invoice->process(1, 2, 3);
+//$invoice->process(15, 'Some description');
 
-echo $service->collectDebt(new \App\Rocky()) . '<br>';
+//$invoice = new \App\Invoice();
+//var_dump($invoice instanceof Stringable);
+
+
+//$invoice = new \App\Invoice();
+//var_dump(is_callable($invoice));
+//$invoice();
+
+$invoice = new \App\Invoice();
+var_dump($invoice);
