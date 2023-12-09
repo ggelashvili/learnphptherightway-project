e4cec40e3350a264a3dfbd1243f7f568b684f1d9
carvalho57 <gabrielfeocarvalho57@gmail.com>
2023-12-09 09:43:38 -0300

    chore: ajusta tabela transactions e remove inserts

--- docker/script.sql
+++ docker/script.sql
@@ -2,63 +2,7 @@ CREATE TABLE
     transactions (
         id INT AUTO_INCREMENT PRIMARY KEY,
         date DATE NOT NULL,
-        check_number INT,
+        check_number VARCHAR(10),
         description VARCHAR(255) NOT NULL,
         amount DECIMAL(10, 2) NOT NULL
-    );
-
-INSERT INTO
-    transactions (
-        date,
-        check_number,
-        description,
-        amount
-    )
-VALUES (
-        '2021-05-01',
-        NULL,
-        'Transaction 2',
-        700.25
-    );
-
-INSERT INTO
-    transactions (
-        date,
-        check_number,
-        description,
-        amount
-    )
-VALUES (
-        '2021-06-01',
-        NULL,
-        'Transaction 3',
-        -1303.97
-    );
-
-INSERT INTO
-    transactions (
-        date,
-        check_number,
-        description,
-        amount
-    )
-VALUES (
-        '2021-07-01',
-        NULL,
-        'Transaction 4',
-        46.78
-    );
-
-INSERT INTO
-    transactions (
-        date,
-        check_number,
-        description,
-        amount
-    )
-VALUES (
-        '2021-08-01',
-        NULL,
-        'Transaction 5',
-        816.87
     );
